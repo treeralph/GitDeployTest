d039331b796d25c11a179b9a3a9158170a064d05
treeralph <treeralph@gmail.com>
2024-10-12 16:03:07 +0900

    first commit

---
+++ deploy/build.gradle.kts
@@ -0,0 +1,51 @@
+import com.android.build.api.dsl.LibraryPublishing
+import com.android.build.gradle.internal.dsl.LibraryPublishingImpl
+import com.android.build.gradle.internal.utils.createPublishingInfoForLibrary
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    `maven-publish`
+}
+
+
+android {
+    namespace = "tree.ralph.deploy"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+group = "com.github.treeralph"
---
+++ deploy/src/main/java/tree/ralph/deploy/MyFirstDeployClass.kt
@@ -0,0 +1,12 @@
+package tree.ralph.deploy
+
+class MyFirstDeployClass {
+
+    private val author = "jun"
+    private val age = 31
+    private val sex = "male"
+
+    fun introduce(): String {
+        return "My name is $author, I am $age years old $sex"
+    }
+}
